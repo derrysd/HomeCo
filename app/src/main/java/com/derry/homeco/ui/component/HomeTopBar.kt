4393a6c2a8e15e7a862bb56cc3878f6270e69043
Derry Surya <derrysd@gmail.com>
2025-05-27 18:36:23 +0700

    - Add dependency injection & navigation libraries - Construct folders into clean architecture structure(ui, data, domain) - Add Nav Graph and Home Screen

---
+++ app/src/main/java/com/derry/homeco/ui/component/HomeTopBar.kt
@@ -0,0 +1,16 @@
+package com.derry.homeco.ui.component
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeTopBar(modifier: Modifier = Modifier) {
+    
+}
+
+
+@Composable
+fun RoundedTitle(modifier: Modifier = Modifier) {
+
+}
